486132e395cca32c424e996f2a4acce9f364861d
Dan Strauss <77133876+DragonMarionette@users.noreply.github.com>
2023-04-09 21:50:46 -0400

    Change default game setup

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,10 @@ mod ai;
 // use crate::{piece::Piece, board::Board};
 
 fn main() {
-    let ai_opponent = AI::new(piece::Piece::X, 100);
-    game::play_human_vs(ai_opponent);
+    // let ai_opponent = AI::new(piece::Piece::X, 100);
+    // game::play_human_vs(ai_opponent);
+
+    let ai_x = AI::new(piece::Piece::X, 100);
+    let ai_o = AI::new(piece::Piece::O, 100);
+    game::play_ai_vs_ai(ai_x, ai_o);
 }
