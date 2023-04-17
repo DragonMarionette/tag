890ff30ace47921bce1a0bea2ce18ac0fcf4bc3c
DragonMarionette <danielmstrauss@gmail.com>
2023-04-17 19:58:01 -0400

    set default behavior to play ai vs human

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 #[allow(unused_imports)]
 use tag::{
     game,
-    Piece,
-    Board,
-    players::{Player, Human, AiParallel, AiSerial},
+    players::{AiLazy, AiParallel, AiSerial, Human, Player},
+    Board, Piece,
 };
 
 fn main() {
     play();
     // let size = 4;
     // let mut ai_x = AiSerial::new(size, Piece::X, 100);
+    // let mut ai_x = AiLazy::new(size, Piece::X);
     // let mut b = Board::new(size);
     // ai_x.make_move(&mut b);
 }
@@ -19,14 +19,16 @@ fn play() {
     let board_size = game::get_board_size();
 
     // let mut p1 = Human::new("Dan", Piece::X);
-    let mut p1 = AiSerial::new(board_size, Piece::X, 100);
-    p1.load_strategy();
+    // let mut p1 = AiSerial::new(board_size, Piece::X, 100);
     // let mut p1 = AiParallel::new(Piece::X, 100);
+    let mut p1 = AiLazy::new(board_size, Piece::X);
+    // p1.load_strategy();
 
     let mut p2 = Human::new("Dan", Piece::O);
     // let mut p2 = AiSerial::new(board_size, Piece::O, 100);
-    // p2.load_strategy();
     // let mut p2 = AiParallel::new(Piece::O, 100);
+    // let mut p2 = AiLazy::new(Piece::O);
+    // p2.load_strategy();
 
     game::play_game(&mut p1, &mut p2, board_size);
     // p1.save_strategy();
